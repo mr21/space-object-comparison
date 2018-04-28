19fbbf2d5555b1afd3d6241dc2c46f5aac9f0fe8
Thomas Tortorini <thomastortorini@gmail.com>
2018-04-28 19:51:07 +0200

    Extract the data

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
 "use strict";
 
-// Data
-// ............................................................................
-const Objects = {
-	    uy_scuti:          { name: "UY Scuty",                        diameter: 1189352136, color: "#ffe2c8", type: "red-supergiant-star" },
-		sagittarius_astar: { name: "Sagittarius A*",                  diameter:   88000000, color: "#000000", type: "black-hole" },
-		alpha_centauri_a:  { name: "α Centauri A",                    diameter:    1704918, color: "#fff5f1", type: "yellow-dwarf-star" },
-		sun:               { name: "Sun",                             diameter:    1391016, color: "#fff5f1", type: "yellow-dwarf-star" },
-		alpha_centauri_b:  { name: "α Centauri B",                    diameter:    1206368, color: "#ffe0bb", type: "yellow-dwarf-star" },
-		alpha_centauri_c:  { name: "α Centauri C (Proxima Centauri)", diameter:     203113, color: "#ffbf68", type: "red-dwarf-star" },
-		jupiter:    { name: "Jupiter",    diameter: 139822, color: "#a4906f", type: "gaz-planet" },
-		saturn:     { name: "Saturn",     diameter: 116464, color: "#e2cb84", type: "gaz-planet" },
-		uranus:     { name: "Uranus",     diameter:  50724, color: "#c1e7ea", type: "gaz-planet" },
-		neptune:    { name: "Neptune",    diameter:  49244, color: "#3d66fa", type: "gaz-planet" },
-		kepler_10c: { name: "Kepler-10c", diameter:  29944, color: "#2c3789", type: "rocky-planet" },
-		earth:      { name: "Earth",      diameter:  12742, color: "#2c3789", type: "rocky-planet" },
-		venus:      { name: "Venus",      diameter:  12104, color: "#d79f3c", type: "rocky-planet" },
-		mars:       { name: "Mars",       diameter:   6779, color: "#ee7f5a", type: "rocky-planet" },
-		mercury:    { name: "Mercury",    diameter:   4879, color: "#828086", type: "iron-planet" },
-		moon:       { name: "Moon",       diameter:   3474, color: "#7f7978", type: "satellite" },
-		pluto:      { name: "Pluto",      diameter:   2377, color: "#866143", type: "dwarf-planet" },
-	},
-	defaultA = "earth",
-	defaultB = "moon";
-
 // <select>
 // ............................................................................
 const selects = document.querySelectorAll( "select" ),
